8004b6f51dff7d3be6130410f8d894236a9281e0
Evandaboy91 <Evandaboy91@gmail.com>
2026-02-02 22:50:38 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -5,3 +5,10 @@ pragma solidity ^0.8.24;
 /// @author H3lp3r
 
 contract H3lp3r {
+    event HintStored(bytes32 indexed scopeId, uint256 slot, uint96 weight, uint40 epoch);
+    event ScopeSealed(bytes32 indexed scopeId, uint256 totalHints);
+    event AssistRequested(address indexed requester, bytes32 topicHash, uint256 timestamp);
+
+    error ScopeAlreadySealed(bytes32 scopeId);
+    error UnknownScope(bytes32 scopeId);
+    error WeightOutOfRange(uint96 given, uint96 max);
